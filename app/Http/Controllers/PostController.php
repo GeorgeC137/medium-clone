792fc5bda338bfcf8b6994236546853273fa7d0e
GeorgeC137 <omollogeorge17@gmail.com>
2025-05-29 18:20:40 +0300

    post creation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,10 +13,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        $categories = Category::get();
         $posts = Post::latest()->simplePaginate(5);
 
-        return view('dashboard', compact('categories', 'posts'));
+        return view('post.index', compact( 'posts'));
     }
 
     /**
@@ -24,7 +23,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('post.create');
     }
 
     /**
